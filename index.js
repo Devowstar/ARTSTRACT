4737de695034b66a6bced1ae7b43b5cde7f51f1e
devonsita <adaparedessolano@crackthecode.la>
2024-11-06 18:50:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import './firebase.js';
 import { auth, db } from './firebase.js';
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
-import { collection, addDoc, getDocs, deleteDoc, updateDoc, doc } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+import { collection, addDoc, getDocs, deleteDoc, updateDoc, doc } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 // Obtener elementos del DOM
 let nombreUsuario = document.getElementById("displayName"); // Nombre del usuario
